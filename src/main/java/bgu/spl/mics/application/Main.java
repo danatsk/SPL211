0b7493196fc8185c9aecc055364d525249238914
korenka <65340580+korenka@users.noreply.github.com>
2020-12-07 23:09:02 +0200

    Added Json parse class. Added some code to Main.java Added relevant dependencies to pom.xml Added getters to Attack.java

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,11 +1,24 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file, 
+import bgu.spl.mics.JsonParse;
+import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.passiveObjects.Attack;
+import bgu.spl.mics.application.services.LeiaMicroservice;
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different components of the application, and run the system.
  * In the end, you should output a JSON.
  */
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		Gson gson = new Gson();
+		Reader reader = new FileReader(args[0]);
+		JsonParse input =gson.fromJson(reader, JsonParse.class);
+		MicroService Leia = new LeiaMicroservice(input.getAttacks());
 	}
 }
